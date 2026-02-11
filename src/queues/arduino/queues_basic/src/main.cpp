42f49b1f40341565c348931c402412aef813049b
yalongwastaken <yalong@gwu.edu>
2026-02-11 15:23:16 -0500

    feat(queue): complete basic implementation for arduino framework

---
+++ src/queues/arduino/queues_basic/src/main.cpp
@@ -0,0 +1,116 @@
+/**
+ * @file main.cpp
+ * @author Anthony Yalong
+ * @brief 
+ */
+
+// imports
+#include <Arduino.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/queue.h"
+#include "driver/gpio.h"
+
+// project configuration
+#define BAUDRATE 115200
+
+// adc configuration
+#define ADC_PIN GPIO_NUM_36
+#define ADC_ATTEN ADC_11db    // 0 - 3.3V
+#define ADC_RESOLUTION 12     // 12 bit resolution
+
+// queue configuration
+#define QUEUE_DEPTH 10
+#define QUEUE_TIMEOUT_MS 100
+
+// task configuration
+#define ADC_TASK_DELAY_MS 500
+
+static TickType_t adc_last_wake;
+static QueueHandle_t event_queue = NULL;
+static const char *TAG = "queue_basic (arduino)";
+
+// function prototypes
+static void adc_task(void *pvParameters);
+static void print_task(void *pvParameters);
+
+void setup() {
+  // error management
+  BaseType_t xRet;
+
+  Serial.begin(BAUDRATE);
+  if (!Serial) delay(10);
+
+  // initialize adc
+  analogSetAttenuation(ADC_ATTEN);
+  analogReadResolution(ADC_RESOLUTION);
+
+
+  // create queue
+  event_queue = xQueueCreate(QUEUE_DEPTH, sizeof(uint16_t));
+  if (event_queue == NULL) {
+    Serial.printf("%s: failed to initialize queue\n", TAG);
+    while (true) {;}  // halt program
+  }
+
+  // create adc task
+  xRet = xTaskCreatePinnedToCore(
+    adc_task,
+    "adc_task",
+    2048,
+    NULL,
+    1,
+    NULL,
+    1
+  );
+  if (xRet != pdTRUE) {
+    Serial.printf("%s: failed to create adc task\n", TAG);
+  }
+
+  // create print task
+  xRet = xTaskCreatePinnedToCore(
+    print_task,
+    "print_task",
+    2048,
+    NULL,
+    1,
+    NULL,
+    1
+  );
+  if (xRet != pdTRUE) {
+    Serial.printf("%s: failed to create print task\n", TAG);
+  }
+}
+
+void loop() {
+  vTaskDelay(portMAX_DELAY);
+}
+
+static void adc_task(void *pvParameters) {
+  uint16_t raw_value;
+  adc_last_wake = xTaskGetTickCount();
+
+  while (true) {
+    raw_value = analogRead(ADC_PIN);
+
+    // enqueue
+    if (xQueueSend(event_queue, &raw_value, pdMS_TO_TICKS(QUEUE_TIMEOUT_MS)) != pdPASS) {
+      Serial.printf("%s: queue full! sample dropped!\n");
+    }
+
+    vTaskDelayUntil(&adc_last_wake, pdMS_TO_TICKS(ADC_TASK_DELAY_MS));
+  }
+}
+
+static void print_task(void *pvParameters) {
+  uint16_t recieved;
+  while (true) {
+    if (xQueueReceive(event_queue, &recieved, portMAX_DELAY) == pdPASS) {
+      // convert to voltage range
+      float voltage = (recieved / 4095.0f) * 3.3f;
+
+      // print
+      Serial.printf("%s: voltage: %0.1f\n", TAG, voltage);
+    }
+  }
+}
